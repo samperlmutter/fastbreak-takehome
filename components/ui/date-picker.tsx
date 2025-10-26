ac074eef254d8c9ef9197eb929869e6192c9988e
Sam Perlmutter <sam.perlmutter227@gmail.com>
2025-10-26 16:56:07 -0400

    fix date picker field height

--- components/ui/date-picker.tsx
+++ components/ui/date-picker.tsx
@@ -32,7 +32,7 @@ export function DatePicker({
         <Button
           variant={"outline"}
           className={cn(
-            "w-full justify-start text-left font-normal",
+            "h-10 w-full justify-start text-left font-normal",
             !date && "text-muted-foreground"
           )}
           disabled={disabled}
